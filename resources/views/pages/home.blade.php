8c758b162646673153926005d596f0b54f0d23f0
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2025-03-14 10:51:53 +0200

    add Arabic language file, homepage skeleton, components, assets, and Bootstrap setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('partials.master')
+@section('page-title', __('keywords.home'))
+
+@section('content')
+
+@endsection
---
+++ resources/views/partials/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('partials.head')
+
+<body>
+    @extends('partials.header')
+    @yield('content')
+
+    @include('partials.scripts')
+</body>
+
+</html>
