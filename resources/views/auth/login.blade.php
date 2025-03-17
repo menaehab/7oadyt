75ab182354d0e64c011504611123d4f32151e6fb
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2025-03-17 19:29:45 +0200

    Setup login and register page and add their functionality

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,28 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+@extends('partials.master')
+@section('page-title', __('Login'))
+@section('content')
+    <div class="auth-wrapper" style="height: 60vh">
+        <div class="auth-container">
+            <form method="POST" action="{{ route('login') }}" class="tab-content bg-white p-4 rounded shadow" dir="rtl">
+                @csrf
+                <h3 class="text-center py-4">{{ __('Login') }}</h3>
+                <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                        <input type="email" name="email" id="loginName" class="form-control" />
+                        <label class="form-label" for="loginName">{{ __('Email') }}</label>
+                    </div>
 
-        <x-validation-errors class="mb-4" />
+                    <!-- Password input -->
+                    <div class="form-outline mb-4">
+                        <input type="password" name="password" id="loginPassword" class="form-control" />
+                        <label class="form-label" for="loginPassword">{{ __('Password') }}</label>
+                    </div>
+                </div>
 
-        @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ $value }}
-            </div>
-        @endsession
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-success btn-block mb-4">{{ __('Login') }}</button>
+            </form>
+        </div>
+    </div>
+@endsection
